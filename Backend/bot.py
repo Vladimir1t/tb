89c5571fbc7e655f0b4ed760f35c48800078bfd4
Vladimir1t <vova.vexov@mail.ru>
2025-08-21 14:13:18 +0300

    добавил бота

--- Backend/bot.py
+++ Backend/bot.py
@@ -1,4 +1,5 @@
 import telebot
+import threading
 
 API_TOKEN = "8143528604:AAEiouPy36hamVNvQhJK3ptZsiaUXJjkwIs"
 bot = telebot.TeleBot(API_TOKEN)
@@ -19,4 +20,15 @@ def bot_get_text_messages(message):
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 def run_bot():
-    bot.polling(none_stop=True)
+    try:
+        bot.polling(non_stop=True, interval=0)
+    except Exception as e:
+        print("Ошибка при запуске бота:", e)
+
+# if __name__ == "__main__":
+#     # Запускаем бота в отдельном потоке
+#     bot_thread = threading.Thread(target=run_bot)
+#     bot_thread.start()
+    
+#     # Держим основной поток живым
+#     bot_thread.join()
