862c29ed8216367e2e0d34f7e5c770e87d7b7ebf
Vladimir1t <vova.vexov@mail.ru>
2025-08-21 13:39:42 +0300

    добавил бота

--- Backend/bot.py
+++ Backend/bot.py
@@ -1,12 +1,20 @@
-import telebot;
-bot = telebot.TeleBot('8143528604:AAEiouPy36hamVNvQhJK3ptZsiaUXJjkwIs');
+import telebot
 
+API_TOKEN = "8143528604:AAEiouPy36hamVNvQhJK3ptZsiaUXJjkwIs"
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(content_types=['text'])
 def bot_get_text_messages(message):
-    """initial bot"""
     if message.text == "/start":
-        bot.send_message(message.from_user.id, "Привет, Это Telegram Catalog. Скорее нажимай на кнопку и ищи нужные тебе каналы и ботов по категориям")
+        bot.send_message(
+            message.from_user.id,
+            "Привет! Это Telegram Catalog. Жми кнопку и ищи каналы/ботов по категориям."
+        )
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Это приложение ищет каналы, ботов и мини приложения по категориям. Жми на кнопку 'Открыть'")
+        bot.send_message(
+            message.from_user.id,
+            "Это приложение ищет каналы, ботов и мини-приложения по категориям."
+        )
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
