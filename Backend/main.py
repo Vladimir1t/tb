f0d43b4d7e41d52b3cf3f29d73c0112bde19e3c7
Vladimir1t <vova.vexov@mail.ru>
2025-10-01 21:54:22 +0300

    test security system

--- Backend/main.py
+++ Backend/main.py
@@ -45,6 +45,5 @@ app.include_router(projects.router, tags=["Projects"])
 app.include_router(users.router, tags=["Users"])
 app.include_router(debug.router, tags=["Debug"])
 
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
